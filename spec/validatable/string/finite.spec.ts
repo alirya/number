f7fe56365862667a52c4ad6a6027f1afb72924eb
dikac <rigel234@yahoo.com>
2022-07-03 20:05:03 +0700

    fix: spec module usage

--- spec/validatable/string/finite.spec.ts
+++ spec/validatable/string/finite.spec.ts
@@ -1,11 +1,11 @@
-import {FiniteParameters} from '../../../dist/assert/string/finite';
+import {FiniteParameter} from '../../../dist/assert/string/finite';
 
 it('enable console log', () => {spyOn(console, 'log').and.callThrough();});
 it(`valid`, () => {
-    expect(FiniteParameters({valid:true, value:Infinity})).toBe('value is finite number.');
+    expect(FiniteParameter({valid:true, value:Infinity})).toBe('value is finite number.');
 });
 
 it(`invalid`, () => {
-    expect(FiniteParameters({valid:false, value:1})).toBe('value must finite number, actual "1".');
+    expect(FiniteParameter({valid:false, value:1})).toBe('value must finite number, actual "1".');
 });
 
